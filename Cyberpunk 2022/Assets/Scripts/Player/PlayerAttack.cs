6479a2e28725db3b9054f5014298a5fcb880ea24
JojuGeorge <jojugeorge001@gmail.com>
2021-08-14 21:53:00 +0530

    Gun aim and shoot while Idle, Walk and Run is working - Basic.

--- Cyberpunk 2022/Assets/Scripts/Player/PlayerAttack.cs
+++ Cyberpunk 2022/Assets/Scripts/Player/PlayerAttack.cs
@@ -26,16 +26,18 @@ public class PlayerAttack : MonoBehaviour
             // if player is not already aiming the gun, then idle -> gun pose + shooting -> aim pose and stops at aim pose
             // bcos if already aiming then no need to do idle -> gun pose :)
             // "PistolFire" == true does idle -> gun pose + shooting -> aim pose
-            if (!_animator.GetBool("AimGun"))
-            {
-                _animator.SetBool("PistolFire", true);
-                _animator.SetBool("AimGun", true);
-            }
+            //if (!_animator.GetBool("AimGun"))
+            //{
+            //    _animator.SetBool("PistolFire", true);
+            //    _animator.SetBool("AimGun", true);
+            //}
             // if aready in gun pose, then just shoot, no need to do idle -> gunpose + shooring
-            else {
-                _animator.SetBool("PistolFire", false);
-                _animator.Play("Shooting_Pistol_02");           // plays the fireing animation
-            }
+            //else {
+            //    _animator.SetBool("PistolFire", false);
+            //    _animator.Play("Shooting_Pistol_02");           // plays the fireing animation
+            //}
+            _animator.Play("Shooting_Pistol_02");           // plays the fireing animation
+
         }
 
 
